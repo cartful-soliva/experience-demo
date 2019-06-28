6e8d1449d587c457940a19c91da6b6a9bb90ce91
Sebastian Oliva <code@sebastianoliva.com>
2019-06-28 03:09:56 -0600

    prog button

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import ProgButton from "../components/progressButton";
+
+function Index() {
+  return (
+    <div>
+      <p>Hello from experience - demo </p>
+      <p> This is a Button</p>
+      <ProgButton  />
+    </div>
+  );
+  
+}
+
+export default Index;
